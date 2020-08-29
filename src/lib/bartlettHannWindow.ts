be01fc5c278bac385360ef656635273bae0bd154
Gustavo Spredemann <gustavo.spredemann@mediamonks.com>
2020-08-29 16:08:23 -0300

    Create bartlettHannWindow.ts

---
+++ src/lib/bartlettHannWindow.ts
@@ -0,0 +1,9 @@
+import { TWO_PI } from './constants'
+
+export const bartlettHannWindow = (length: number, index: number) => {
+  return (
+    0.62 -
+    0.48 * Math.abs(index / (length - 1) - 0.5) -
+    0.38 * Math.cos((TWO_PI * index) / (length - 1))
+  )
+}
