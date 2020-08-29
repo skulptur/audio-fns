f398fa3c95d0a91306843065f1ad8ba6ab63f1dc
Gustavo Spredemann <gustavo.spredemann@mediamonks.com>
2020-08-29 16:08:12 -0300

    Create blackmanWindow.ts

---
+++ src/lib/blackmanWindow.ts
@@ -0,0 +1,17 @@
+import { TWO_PI } from './constants'
+
+export const blackmanWindow = (
+  length: number,
+  index: number,
+  alpha: number
+) => {
+  const a0 = (1 - alpha) / 2
+  const a1 = 0.5
+  const a2 = alpha / 2
+
+  return (
+    a0 -
+    a1 * Math.cos((TWO_PI * index) / (length - 1)) +
+    a2 * Math.cos((4 * Math.PI * index) / (length - 1))
+  )
+}
