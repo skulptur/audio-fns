8ddc7a639179bf49b6ca4b21f121269ee79ed913
Gustavo Spredemann <gustavo.spredemann@mediamonks.com>
2020-08-29 15:50:09 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,13 @@
+// single sample processing
+export { invert } from './lib/invert'
+
+// conversion
 export { hertzToMel } from './lib/hertzToMel'
 export { hertzToOctaves } from './lib/hertzToOctaves'
-export { invert } from './lib/invert'
 export { melToHertz } from './lib/melToHertz'
+
+// waves
+export { sawWave } from './lib/sawWave'
+export { sineWave } from './lib/sineWave'
+export { squareWave } from './lib/squareWave'
+export { triangleWave } from './lib/triangleWave'
