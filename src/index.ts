5d75c12caa085fd7540fd1b6f6cf458fc864a963
Gustavo Spredemann <gustavo.spredemann@mediamonks.com>
2020-08-29 15:26:39 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,4 @@
-export const sum = (a: number, b: number) => {
-  if ('development' === process.env.NODE_ENV) {
-    console.log('boop')
-  }
-  return a + b
-}
+export { hertzToMel } from './lib/hertzToMel'
+export { hertzToOctaves } from './lib/hertzToOctaves'
+export { invert } from './lib/invert'
+export { melToHertz } from './lib/melToHertz'
