bf7851e95cd127774013215ea709ac993f81410f
Gustavo Spredemann <gustavo.spredemann@mediamonks.com>
2020-09-05 22:40:45 -0300

    Update bpmToSamples.test.ts

--- src/lib/conversion/bpmToSamples.test.ts
+++ src/lib/conversion/bpmToSamples.test.ts
@@ -1,7 +1,7 @@
 import { bpmToSamples } from './bpmToSamples'
 
 describe('bpmToSamples', () => {
-  it('converts 0..1 to decibels', () => {
+  it('converts BPM to samples', () => {
     const bpms = [30, 60, 120, 240]
 
     const result = bpms.map(bpm => bpmToSamples(44100, bpm))
