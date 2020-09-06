0098a43386f5f66193ce4d91e67eff2c4190c7e5
Gustavo Spredemann <gustavo.spredemann@mediamonks.com>
2020-09-05 22:43:32 -0300

    use array for state

--- src/lib/synthesis/pinkNoise.ts
+++ src/lib/synthesis/pinkNoise.ts
@@ -1,14 +1,6 @@
 import { whiteNoise } from './whiteNoise'
 
-const initialPinkNoiseState = {
-  b0: 0,
-  b1: 0,
-  b2: 0,
-  b3: 0,
-  b4: 0,
-  b5: 0,
-  b6: 0,
-}
+const initialPinkNoiseState = [0, 0, 0, 0, 0, 0, 0]
 
 export const pinkNoise = (
   lengthInSamples: number,
@@ -20,23 +12,23 @@ export const pinkNoise = (
 
   for (let i = 0; i < lengthInSamples; i++) {
     const white = whiteNoiseGenerator()
-    state.b0 = 0.99886 * state.b0 + white * 0.0555179
-    state.b1 = 0.99332 * state.b1 + white * 0.0750759
-    state.b2 = 0.969 * state.b2 + white * 0.153852
-    state.b3 = 0.8665 * state.b3 + white * 0.3104856
-    state.b4 = 0.55 * state.b4 + white * 0.5329522
-    state.b5 = -0.7616 * state.b5 - white * 0.016898
+    state[0] = 0.99886 * state[0] + white * 0.0555179
+    state[1] = 0.99332 * state[1] + white * 0.0750759
+    state[2] = 0.969 * state[2] + white * 0.153852
+    state[3] = 0.8665 * state[3] + white * 0.3104856
+    state[4] = 0.55 * state[4] + white * 0.5329522
+    state[5] = -0.7616 * state[5] - white * 0.016898
     const val =
-      state.b0 +
-      state.b1 +
-      state.b2 +
-      state.b3 +
-      state.b4 +
-      state.b5 +
-      state.b6 +
+      state[0] +
+      state[1] +
+      state[2] +
+      state[3] +
+      state[4] +
+      state[5] +
+      state[6] +
       white * 0.5362
     buffer[i] = val * 0.11
-    state.b6 = white * 0.115926
+    state[6] = white * 0.115926
   }
 
   return {
