f89735fa812c71ab1c6d96d15db469840884a46e
Gustavo Spredemann <gustavo.spredemann@mediamonks.com>
2020-09-07 22:07:05 -0300

    update oscillator

--- src/lib/synthesis/oscillator.ts
+++ src/lib/synthesis/oscillator.ts
@@ -1,9 +1,26 @@
-export const oscillator = (
+//  const oscillator = (
+//   sampleRate: number,
+//   periodicFn: (t: number) => number,
+//   pitch: number,
+//   currentSample: number
+// ) => {
+//   const t = ((currentSample + 2 * pitch) % sampleRate) / sampleRate
+//   return periodicFn(t)
+// }
+
+const pitchRate = (
   sampleRate: number,
-  periodicFn: (t: number) => number,
-  pitch: number,
-  currentSample: number
+  currentSample: number,
+  pitch: number
 ) => {
-  const t = ((currentSample + 2 * pitch) % sampleRate) / sampleRate
-  return periodicFn(t)
+  return ((currentSample % sampleRate) / sampleRate) * pitch
+}
+
+export const oscillator = (
+  generatorFn: (t: number) => number,
+  sampleRate: number,
+  currentSample: number,
+  pitch: number
+) => {
+  return generatorFn(pitchRate(sampleRate, currentSample, pitch))
 }
